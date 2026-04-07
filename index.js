c0fec61a32f85732866210910b01bd40b590e40a
akashzone <akashnadar.dev@gmail.com>
2026-04-07 21:07:08 +0530

    Install ejs-mate to create templates

--- index.js
+++ index.js
@@ -3,13 +3,17 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const ejsMate = require('ejs-mate');
 
 
 const path = require('path');
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(express.urlencoded({extended: true}));
+app.engine('ejs', ejsMate);
 
 
 const methodOverride = require('method-override');
